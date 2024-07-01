f5148bd8b2a9573d62844d9960a1e703adf045f3
yaorp <yao_rp@qq.com>
2024-07-01 17:34:19 +0800

    初始化

---
+++ sc01-eureka/eureka-server/src/main/java/com/springcloud/ms/EurekaServerApplication.java
@@ -0,0 +1,16 @@
+package com.springcloud.ms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaServerApplication.class);
+        System.out.println("【【EurekaServerApplication】】");
+    }
+
+}
